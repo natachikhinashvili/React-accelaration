5a1bb7c73bea68e6a814e91f57fbd6df025584f4
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-09-23 18:27:19 +0400

    feat: currency init

--- country-app/src/Currency.js
+++ country-app/src/Currency.js
@@ -1,44 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Currency(){
-  const [exchangeRates, setExchangeRates] = useState(null);
-  const [error, setError] = useState(null);
-
+  const [from, setFrom] = useState('USD');
+  const [to, setTo] = useState('');
+  const [res, setres] = useState(0)
+  const [amount, setAmount] = useState(0);
+  async function getConvertedData(){
+    const API_URL = `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`;
+    const result = await fetch(API_URL);
+    const data = await result.json();
+    setres(data.result)
+  }
+  getConvertedData();
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://exchangerate.host/');
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setExchangeRates(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setError('No Data Found 😟');
-      }
-    };
-    const timeout = setTimeout(() => {
-      setError('Request timed out. Please try again later. 😟');
-    }, 10000);
-    fetchData();
-    return () => clearTimeout(timeout);
+
   }, []);
 
   return (
     <div id='currency-container'>
       <h1>Currency Exchange</h1>
-      {error ? (
-        <p>{error}</p>
-      ) : exchangeRates && (
-        <ul>
-          {Object.keys(exchangeRates.rates).map((currency) => (
-            <li key={currency}>
-              {currency}: {exchangeRates.rates[currency]}
-            </li>
-          ))}
-        </ul>
-      )}
+      <label>
+          Amount:
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </label>
+      <input value={to} onChange={(e) => setTo(e.target.value)}/>
+      <button onClick={getConvertedData}>convert</button>
+      <p>{res}</p>
     </div>
   );
 };
