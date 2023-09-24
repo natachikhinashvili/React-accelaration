403e2afd9a9f5a854f1a4db110f3dca607de93b6
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-09-24 16:30:30 +0400

    fix: rerendering errors and navigating errors

--- country-app/src/Home.js
+++ country-app/src/Home.js
@@ -1,28 +1,49 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from './Navigation';
 import Country from "./Country";
 import { useNavigate } from "react-router-dom";
 import { Outlet } from 'react-router-dom';
-import * as React from 'react';
 import { useRef } from 'react';
 
-export default function Home(props){
-    const [countries, setCountres] = useState(false);
-    const [selectedCountry, setSelectedCountry] = useState(false);
-    const navigate = useNavigate();
-    const [error, setError] = useState('')
-    const cache = useRef({})
-  
-    const handleSelectChange = (event) => {
-        let index = event.target.selectedIndex -1;
-        setSelectedCountry(countries[index]);
-    };
-    useEffect(() => {
-      if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const { latitude, longitude } = position.coords;
-            const cacheKey = `${latitude}-${longitude}`;
+export default function Home() {
+  const [countries, setCountries] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [isSelected, setIsSelected] = useState(false);
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const cache = useRef({});
+
+  // Effect to fetch countries data
+  useEffect(() => {
+            if (selectedCountry) {
+            navigate(`/${selectedCountry.cca2}`);
+          }
+    fetch('https://restcountries.com/v3.1/all')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setCountries(data);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }, []);
+
+  // Effect to handle geolocation
+  useEffect(() => {
+    if (selectedCountry) {
+      navigate(`/${selectedCountry.cca2}`);
+    }
+    if ("geolocation" in navigator && countries && !isSelected) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          const cacheKey = `${latitude}-${longitude}`;
+          
           // Check if the response is cached
           if (cache.current[cacheKey]) {
             setSelectedCountry(cache.current[cacheKey]);
@@ -36,63 +57,52 @@ export default function Home(props){
               })
               .then(data => {
                 if (data.status !== "REQUEST_DENIED") {
-                  console.log(data);
-                  setSelectedCountry(data);
+                  const foundCountry = countries.find(element => element.name.common === data.results[data.results.length - 1].formatted_address);
+                  setSelectedCountry(foundCountry);
                   cache.current[cacheKey] = data; // Cache the response
+                  navigate(`/${foundCountry.cca2}`);
                 } else {
                   setError(data);
                 }
               })
               .catch(error => {
                 console.error('Fetch error:', error);
-              })
-            };
-          },
-          (error) => {
-            setError(error);
+              });
           }
-        );
-      } else {
-        setError('Geolocation is not available in this browser.');
-      }
-        if (selectedCountry) {
-            navigate(`/${selectedCountry.cca2}`);
-          }
-        fetch('https://restcountries.com/v3.1/all')
-            .then(response => {
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-              return response.json();
-            })
-            .then(data => {
-              setCountres(data)
-            })
-            .catch(error => {
-              console.error('Fetch error:', error);
-            });
+        },
+        (error) => {
+          setError(error);
+        }
+      );
+    } else {
+      setError('Geolocation is not available in this browser.');
+    }
+  }, [countries, isSelected, navigate]);
 
-        }, [selectedCountry, navigate])
-  
-    return (
-      <div className="App">
-        <div id='container'>
+  // Function to handle select change
+  const handleSelectChange = (event) => {
+    const index = event.target.selectedIndex - 1;
+    setIsSelected(true);
+    setSelectedCountry(countries[index]);
+  };
+
+  return (
+    <div className="App">
+      <div id='container'>
         <select id="country-dropdown" onChange={handleSelectChange}>
-          <option value="">Select a country</option> 
+          <option value="">Select a country</option>
           {countries ? countries.map((country, index) => (
-            <option key={index} value={index}>{country.name.common}
-            </option>
-          )): <p>Loading...</p>}
-          </select>
-          <div id='country_info'>
-            <Country props={selectedCountry}/>
-          </div>
-
-          <div>
-            <Navigation props={selectedCountry}/>
-          <Outlet/>
-          </div>
+            <option key={index} value={index}>{country.name.common}</option>
+          )) : <p>Loading...</p>}
+        </select>
+        <div id='country_info'>
+          <Country props={selectedCountry} />
+        </div>
+        <div>
+          <Navigation props={selectedCountry} />
+          <Outlet />
         </div>
       </div>
-    );
+    </div>
+  );
 }
